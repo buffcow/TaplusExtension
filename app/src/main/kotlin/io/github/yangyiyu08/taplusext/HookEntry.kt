397b9d5656128dc9ea5a1862b3669b582f81a513
yangyiyu08 <qyma.yyy@qq.com>
2023-05-09 02:37:12 +0800

    - optimize code

--- app/src/main/kotlin/io/github/yangyiyu08/taplusext/HookEntry.kt
+++ app/src/main/kotlin/io/github/yangyiyu08/taplusext/HookEntry.kt
@@ -4,6 +4,9 @@ import com.highcapable.yukihookapi.annotation.xposed.InjectYukiHookWithXposed
 import com.highcapable.yukihookapi.hook.factory.configs
 import com.highcapable.yukihookapi.hook.factory.encase
 import com.highcapable.yukihookapi.hook.xposed.proxy.IYukiHookXposedInit
+import hooker.RecognitionHooker
+import hooker.SettingsHoker
+import hooker.UtilsHoker
 
 
 //
